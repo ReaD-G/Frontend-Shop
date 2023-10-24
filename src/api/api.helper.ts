e92618b4eeae69b4617ea69403379e5b3afaf3b9
Egor <readg.kill@mail.ru>
2023-10-24 15:24:24 +0300

    test deploy

--- src/api/api.helper.ts
+++ src/api/api.helper.ts
@@ -1,10 +1,7 @@
 export const getContentType = () => ({
 	'Content-Type': 'application/json',
-	'Access-Control-Allow-Origin': 'http://localhost:3000',
-	'Access-Control-Allow-Credentials': 'true',
-	'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT',
-	'Access-Control-Allow-Headers':
-		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'GET,DELETE,PATCH,POST,PUT,OPTIONS'
 })
 
 export const errorCatch = (error: any): string => {
